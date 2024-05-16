b945d56c4b77af3a7985e0aefa2e725fc0fd47d8
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-16 16:55:17 +0100

    Todo completing functionality added

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,13 +1,26 @@
 import styles from "./toDoItem.module.css";
 export default function ToDoItem({ item, toDos, setToDos }) {
   function toDoDelete(item) {
-    console.log("Deleting item",item);
+    console.log("Deleting item", item);
     setToDos(toDos.filter((toDo) => toDo !== item));
-    
   }
+
+  function handleClick(name) {
+    console.log("Item click", name);
+    setToDos(
+      toDos.map((toDo) =>
+        toDo.name === name ? { ...toDo, done: !toDo.done } : toDo
+      )
+    );
+  }
+  const classCompleted = item.done ? styles.completed : "";
   return (
     <div className={styles.toDoItemWrapper}>
-      <div className={styles.toDoItem}>{item.name}</div>
+      <div className={styles.toDoItem}>
+        <span onClick={() => handleClick(item.name)} className={classCompleted}>
+          {item.name}
+        </span>
+      </div>
       <span>
         <button onClick={() => toDoDelete(item)}>X</button>
       </span>
