ef7bad89c29400219efb7978581407c84f2658e5
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-16 01:39:52 +0100

    Todo list styles updated

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -3,6 +3,7 @@ export default function ToDoItem({ item }) {
   return (
     <div className={styles.toDoItemWrapper}>
       <div className={styles.toDoItem}>{item}</div>
+      <span><button>X</button></span>
     </div>
   );
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,9 +4,9 @@ import styles from "./toDoList.module.css";
 export default function ToDoList({ toDos }) {
   return (
     <div className={`${styles.todoList} container`}>
-      {toDos.map((item) => (
-        <ToDoItem key={item} item={item} />
-      ))}
+        {toDos.map((item) => (
+          <ToDoItem key={item} item={item} />
+        ))}
     </div>
   );
 }
