a922463dca3de9edbfd0868ffe1d9c679a1ba29f
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-17 01:28:51 +0100

    ToDoStat styles updated

--- src/components/ToDoStats.jsx
+++ src/components/ToDoStats.jsx
@@ -2,8 +2,16 @@ import styles from "./toDoStats.module.css";
 export default function ToDoStats({ totalToDos, completedToDos }) {
   return (
     <div className={`container ${styles.toDoStatsWrapper}`}>
-      <h5>Total: {totalToDos}</h5>
-      <h5>Completed: {completedToDos}</h5>
+      <div className={styles.statWrapper}>
+        <span>
+          Total Tasks: <b>{totalToDos}</b>
+        </span>
+      </div>
+      <div className={styles.statWrapper}>
+        <span>
+          Completed Tasks: <b>{completedToDos}</b>
+        </span>
+      </div>
     </div>
   );
 }
