fbf8dc82bb8ffb5b4a687aaf4d13e15761dbff1f
Sankalpa Senevirathne <dev.sankalpa97@gmail.com>
2024-05-17 01:55:12 +0100

    TaskFlow logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import styles from "./header.module.css";
+import taskflowlogo from "../assets/taskflow.png";
 export default function Header() {
   return (
     <div className={`${styles.header} container`}>
-      <h1 className="text-green-primary">TaskFlow 🚀</h1>
-      <h4 className="text-green-primary">Type your list item & just hit 'Add'.</h4>
+      <div className={styles.headerLogoWrapper}>
+        <img
+          src={taskflowlogo}
+          alt="TaskFlow Logo"
+          className={styles.headerLogo}
+        />
+        <h1 className="text-green-primary">TaskFlow</h1>
+      </div>
+      <h4 className="text-green-primary">
+        Type your list item & just hit 'Add'.
+      </h4>
     </div>
   );
 }
